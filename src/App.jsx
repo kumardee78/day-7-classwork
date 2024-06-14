b5379d038e572614cfdff0b64877fd3dbc90891a
kumardee78 <deedancer951@gmail.com>
2024-06-14 11:42:43 +0530

    css and apply local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { FaTrash } from "react-icons/fa";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputValue, setInputValue] = useState("");
+  const [task, setTask] = useState([]);
+
+  function handleTask() {
+    if (inputValue === "") {
+      alert("Please Enter your Task");
+    } else {
+      let obj = {};
+      obj.task = inputValue;
+      obj.id = Date.now();
+      setTask([...task, obj]);
+    }
+    setInputValue("");
+  }
+
+  function handleDelete(index) {
+    setTask(
+      task.filter((task) => {
+        return task.id !== index;
+      })
+    );
+  }
+  
+  useEffect(() => {
+    const storedTasks = JSON.parse(localStorage.getItem('tasks'));
+    if (storedTasks) {
+      setTask(storedTasks);
+    }
+}, []);
+
+// Save tasks to localStorage whenever tasks change
+useEffect(() => {
+  if(task.length > 0) localStorage.setItem('tasks', JSON.stringify(task));
+}, [task]);
+
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="my-32 py-8 w-1/2 mx-auto text-center shadow-xl bg-[whiteSmoke]">
+      <h1 className="text-3xl font-bold mb-8">Glossery Buds</h1>
+      <input
+        className="border-2 mr-6 w-[16rem] px-2 py-1"
+        type="text"
+        placeholder="Add a task"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button
+        onClick={handleTask}
+        className="border py-1 px-4 bg-red-600 text-white"
+      >
+        Add task
+      </button>
+      <div className="py-4 border my-4 text-start px-2 w-1/2 mx-auto">
+        {task.map((item) => {
+          return (
+            <p key={item.id} className="flex gap-8">
+              {item.task}
+              <FaTrash onClick={() => handleDelete(item.id)} />
+            </p>
+          );
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
